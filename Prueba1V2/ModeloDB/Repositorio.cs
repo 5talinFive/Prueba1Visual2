8251eebec6686b6eae4bcd6153e2bb9ff1bdc258
5talin 5 <59964372+5talinFive@users.noreply.github.com>
2022-01-24 19:43:36 -0500

    Add files via upload

---
+++ Prueba1V2/ModeloDB/Repositorio.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modelo;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore.tools;
+
+namespace ModeloDB
+{
+    public class Repositorio : DbContext
+    {
+        public DbSet<address> address { get; set; }
+        public DbSet<payment> payment { get; set; }
+        public DbSet<staff> staff { get; set; }
+
+        //Configuracion de la conexion
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=MIPCC\\STALINSQL; Initial Catalog=store; trusted_connection=true;");
+
+
+        }
+    }
+}
